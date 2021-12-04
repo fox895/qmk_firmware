a62710cae00ac7f41b9b93d6c8c9bf037ffab766
Matteo Altorio <m.altorio@gmail.com>
2021-12-04 10:20:11 +0000

    Added flags for smaller firmware

--- keyboards/lily58/keymaps/fox895/rules.mk
+++ keyboards/lily58/keymaps/fox895/rules.mk
@@ -16,7 +16,7 @@ OLED_ENABLE= yes     # OLED display
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-
+EXTRAFLAGS += -flto
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
