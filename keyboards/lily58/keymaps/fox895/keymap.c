2db8aac5724649162f034825b89fa2d0fe78adb2
Matteo Altorio <m.altorio@gmail.com>
2022-01-29 03:04:38 +0000

    Improved symbol layout

--- keyboards/lily58/keymaps/fox895/keymap.c
+++ keyboards/lily58/keymaps/fox895/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -26,11 +26,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_QWERTY] = LAYOUT( \
-  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                    KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV,
+  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                    KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,
   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
   KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                   KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
   KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                 KC_LALT, MO(_SYMBOLS), KC_LGUI, KC_ENT, KC_SPC, KC_BSPC, MO(_ARROW), MO(_MEDIA)
+         KC_LALT, MO(_SYMBOLS), KC_LGUI, KC_ENT,  KC_SPC,  KC_BSPC, MO(_ARROW), MO(_MEDIA)
   ),
 
 
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      |   ^  |   %  |   @  |   <  |                    |   >  |   #  |   $  |   !  |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   `  |   {  |   [  |   (  |   :  |-------.    ,-------|   =  |   )  |   ]  |   }  |   '  |   ~  |
+ * |      |   `  |   {  |   [  |   (  |   :  |-------.    ,-------|   =  |   )  |   ]  |   }  |   \  |   '  |
  * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
- * |      |   !  |   @  |   #  |   $  |   -  |-------|    |-------|   _  |   .  |   ,  |   *  |   /  |      |
+ * |      |      |      |      |      |   -  |-------|    |-------|   _  |   *  |   ,  |   .  |   /  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \      \  |  DEL |FUNCTS|      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -52,10 +52,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_SYMBOLS] = LAYOUT( \
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_GRV,  KC_LCBR, KC_LBRC, KC_LPRN, KC_COLN,                   KC_EQL,   KC_RPRN, KC_RBRC, KC_RCBR, KC_QUOT, KC_TILD,
-KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_MINS, KC_LT,   KC_GT, KC_UNDS, KC_DOT, KC_COMM, KC_ASTR, KC_SLSH, KC_TRNS,
-                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, MO(_FUNCTIONS), KC_TRNS
+KC_NO,   KC_NO,   KC_CIRC, KC_PERC, KC_AT,   KC_LT,                     KC_GT,   KC_HASH, KC_DLR,  KC_EXLM, KC_NO,   KC_NO,
+KC_TRNS, KC_GRV,  KC_LCBR, KC_LBRC, KC_LPRN, KC_COLN,                   KC_EQL,  KC_RPRN, KC_RBRC, KC_RCBR, KC_BSLS, KC_QUOT,
+KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_LT,   KC_GT,   KC_UNDS, KC_ASTR, KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
+                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,  MO(_FUNCTIONS),   KC_TRNS
 ),
 
 
@@ -64,11 +64,11 @@ KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_MINS, KC_LT,   KC_GT, KC_UNDS, K
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |   6  |   7  |   8  |   9  |   0  |                    |      |   +  |   *  |   -  |   /  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   1  |   2  |   3  |   4  |   5  |-------.    ,-------| Left | Down |  Up  | Right|   :  |      |
+ * |      |   1  |   2  |   3  |   4  |   5  |-------.    ,-------| Left | Down |  Up  | Right|   :  |   =  |
  * |------+------+------+------+------+------|       |    |   \   |------+------+------+------+------+------|
- * |      |   6  |   7  |   8  |   9  |   0  |-------|    |-------|   +  |  -   |  =   |  [   |  ]   |      |
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |FUNCT |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -77,9 +77,9 @@ KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_MINS, KC_LT,   KC_GT, KC_UNDS, K
 
 [_ARROW] = LAYOUT( \
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_COLN, KC_NO,
-KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_TRNS, KC_BSLS, KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_TRNS,
+KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                   KC_NO,   KC_PLUS, KC_ASTR, KC_PMNS, KC_PSLS, KC_NO,
+KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_COLN, KC_PEQL,
+KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_TRNS, KC_BSLS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                            KC_TRNS, MO(_FUNCTIONS), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
@@ -113,7 +113,7 @@ KC_TRNS, KC_NO, KC_NO, KC_END, KC_PGDN,KC_NO, KC_F6,   KC_F7, KC_NO,   KC_PGUP, 
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      | Mute | Vol- | Vol+ |      |-------.    ,-------|      |      | Prev | Play | Next | Calc |
+ * |C_LOCK|      | Mute | Vol- | Vol+ |      |-------.    ,-------|      |      | Prev | Play | Next | Calc |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -122,11 +122,11 @@ KC_TRNS, KC_NO, KC_NO, KC_END, KC_PGDN,KC_NO, KC_F6,   KC_F7, KC_NO,   KC_PGUP, 
  *                   `----------------------------'           '------''--------------------'
  */
 [_MEDIA] = LAYOUT( \
-KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,                KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_CALC,
-KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                       KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_TRNS
+KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
+KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
+KC_CAPS, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,                KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_CALC,
+KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                         KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_TRNS
 ),
 
 
